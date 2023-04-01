d71224f45465983dc7b01c956d04f8408f01e7dd
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-01 22:11:05 +0600

    Add manually install react,webpack,babel etc

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles/app.css';
+
+
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(<h1>Hello from React application</h1>);
